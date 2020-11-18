6f0ae725b325ad93cc0affa881f2e748f93be21e
Alex Gong <43153080+pyzander@users.noreply.github.com>
2020-11-17 19:16:10 -0600

    Made first draft. ToDo: fix nav bar, add classes and standardize styling throughout

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add any homepage scripting here!
+//want to return collection of elements
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+//on click, you can view whatever is in the accordion
+//the reason we put this in a loop is if there is multiple elements of type accordion
